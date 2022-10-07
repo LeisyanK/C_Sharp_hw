8a11de558f6148ea0857bae7c4aacd584e7fba6d
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 12:15:16 +0500

    Task0

---
+++ task0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+            int sqr = number * number;
+            Console.WriteLine("Квадрат числа " + number + " равен " + sqr);
+
+        }
+    }
+}
