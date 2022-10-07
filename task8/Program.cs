d188b4c916919c30ec7f86b84ef899921cc5f4b6
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 20:37:30 +0500

    Task8

---
+++ task8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Четные числа в диапазоне от 1 до " + N + ":");
+            for (int i=2; i<=N; i+=2)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
