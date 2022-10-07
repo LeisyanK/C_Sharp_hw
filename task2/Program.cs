b0e26887b6ae5f8cc79e3df9e8bd41cfd200166a
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 20:06:04 +0500

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -10,7 +10,8 @@ namespace task2
             int number1 = int.Parse(Console.ReadLine());
             int number2 = int.Parse(Console.ReadLine());
 
-            if (number1 > number2) Console.WriteLine(number1 + " больше " + number2);
+            if (number1 == number2) Console.WriteLine("Числа равны");
+            else if (number1 > number2) Console.WriteLine(number1 + " больше " + number2);
             else Console.WriteLine(number2 + " больше " + number1);
         }
     }
