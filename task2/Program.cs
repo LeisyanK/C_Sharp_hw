04b8ec3dca7f0c0f74a572ec1be470619fa1a988
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 19:59:55 +0500

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа:");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2) Console.WriteLine(number1 + " больше " + number2);
+            else Console.WriteLine(number2 + " больше " + number1);
+        }
+    }
+}
