774fedb4e6db87a15dbffda7fdfa9ab93a8334e2
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 19:42:56 +0500

    Task7

---
+++ task7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 99 && number < 1000)
+            {
+                Console.Write("Последняя цифра числа " + number);
+                number = number % 10;
+                Console.WriteLine(" равна " + number);
+            }
+            else 
+            {
+                Console.WriteLine("Вы ввели неправильное число");
+            }
+        }
+    }
+}
