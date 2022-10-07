bff3e85e6d020a0a096497f08ac8d08e5f6460fe
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 18:37:56 +0500

    Task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа: ");
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            if (number1 == number2 * number2)
+            {
+                Console.WriteLine("Число " + number1 + " является квадратом числа " + number2);
+            }
+            else
+            {
+                Console.WriteLine("Число " + number1 + " не является квадратом числа " + number2);
+            }
+        }
+    }
+}
