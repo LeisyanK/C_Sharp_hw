29a9a3bd5c4e593c491b0eb6b8855a40d10a4b5b
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-07 19:29:30 +0500

    Task5

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int i = N * (-1);
+            while (i <= N)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+        }
+    }
+}
