a51ac577e4c3b67a1e96aae9a01682653ca625fe
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-11 14:59:55 +0500

    task 9

---
+++ task9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Наибольшая цифра двузначного числа
+            Console.Clear();
+            int num = new Random().Next(10, 100);
+            int num1 = num / 10;
+            int num2 = num % 10;
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"Наибольшая цифра числа {num} равна {num1}");
+            }
+            else Console.WriteLine($"Наибольшая цифра числа {num} равна {num2}");
+        }
+    }
+}
