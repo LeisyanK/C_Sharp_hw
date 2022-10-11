f4bbcd16db390a976b6694346104cc74f988bc1c
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-11 15:34:18 +0500

    task 15

---
+++ task15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 1 || num > 7)
+            {
+                Console.WriteLine("Вы ввели некорректное число");
+            }
+            else
+            {
+                if (num == 6 || num == 7) 
+                {
+                    Console.WriteLine("Выходной день");
+                }
+                else
+                {
+                    Console.WriteLine("Будний день");
+                }
+            }
+        }
+    }
+}
