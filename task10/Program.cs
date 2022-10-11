4f26e02e219f067013abfc4687d28dacc70bf1c2
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-11 15:10:53 +0500

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 100 || num > 999)
+            {
+                Console.WriteLine("Вы ввели некорректное число");
+            }
+            else
+            {
+                int num1 = num / 10;
+                int num2 = num1 % 10;
+                Console.WriteLine($"Вторая цифра числа {num} равна {num2}");
+            }
+        }
+    }
+}
