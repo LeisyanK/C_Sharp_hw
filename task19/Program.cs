956873344dae94c704afbba4aebc2fe41e9ef1b9
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-13 21:11:15 +0500

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите пятизначное число: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if ((num % 10 == num / 10000) && (num % 100 / 10 == num / 1000 % 10))
+            {
+                /*Console.WriteLine(num % 10);
+                Console.WriteLine(num / 10000);
+                Console.WriteLine(num % 100 / 10);
+                Console.WriteLine(num / 1000 % 10);*/
+                Console.WriteLine("Число является палиндромом");
+            }
+            else 
+            {
+                Console.WriteLine("Число не является палиндромом");
+            }
+        }
+    }
+}
