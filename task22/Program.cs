e2813da2b08004cb7db152a350364fc437035cf6
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-13 21:17:09 +0500

    task22

---
+++ task22/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i < num + 1; i++)
+            {
+                Console.Write($"{i*i} ");
+            }
+        }
+    }
+}
