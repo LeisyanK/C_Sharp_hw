cd8048a2e02bbeabde6a428ed7431b368a369d80
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-13 23:07:37 +0500

    task19 with method

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,21 +6,35 @@ namespace task19
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите пятизначное число: ");
-            int num = int.Parse(Console.ReadLine());
-
-            if ((num % 10 == num / 10000) && (num % 100 / 10 == num / 1000 % 10))
+            void Palindrom(int number)
             {
-                /*Console.WriteLine(num % 10);
-                Console.WriteLine(num / 10000);
-                Console.WriteLine(num % 100 / 10);
-                Console.WriteLine(num / 1000 % 10);*/
+                if ((number % 10 == number / 10000) && (number % 100 / 10 == number / 1000 % 10))
+            {
                 Console.WriteLine("Число является палиндромом");
             }
             else 
             {
                 Console.WriteLine("Число не является палиндромом");
             }
+            }
+
+            Console.Write("Введите пятизначное число: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Palindrom(num);
+/*
+            if ((num % 10 == num / 10000) && (num % 100 / 10 == num / 1000 % 10))
+            {
+                /*Console.WriteLine(num % 10);
+                Console.WriteLine(num / 10000);
+                Console.WriteLine(num % 100 / 10);
+                Console.WriteLine(num / 1000 % 10);*/
+/*                Console.WriteLine("Число является палиндромом");
+            }
+            else 
+            {
+                Console.WriteLine("Число не является палиндромом");
+            }*/
         }
     }
 }
