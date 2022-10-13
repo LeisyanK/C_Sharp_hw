df647e5c03796c1cb32877ff666d3fb1470f11e5
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-13 23:28:47 +0500

    task23 with method

--- task23/Program.cs
+++ task23/Program.cs
@@ -6,14 +6,23 @@ namespace task23
     {
         static void Main(string[] args)
         {
+            void Kub(int n)
+            {
+                for (int i=1; i < n+1; i++)
+                {
+                    Console.Write(i*i*i + " ");
+                }
+            }
+
             Console.WriteLine("Введите число:");
             int N = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Кубы чисел от 1 до " + N + ":");
-            for (int i=1; i < N+1; i++)
+            Kub(N);
+            /*for (int i=1; i < N+1; i++)
             {
                 Console.Write(i*i*i + " ");
-            }
+            }*/
         }
     }
 }
