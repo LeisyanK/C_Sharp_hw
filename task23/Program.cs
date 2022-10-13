d4fc1c7519cf299c65210ee42485996f5095c765
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-13 21:21:44 +0500

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Кубы чисел от 1 до " + N + ":");
+            for (int i=1; i < N+1; i++)
+            {
+                Console.Write(i*i*i + " ");
+            }
+        }
+    }
+}
