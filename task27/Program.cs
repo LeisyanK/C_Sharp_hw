d50d2de415016c866645126aab6f636a48d76c54
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-14 12:37:21 +0500

    task27 homework

---
+++ task27/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите программу, которая принимает на 
+вход число и выдаёт сумму цифр в числе.*/
+            int SumNums(int number)
+            {
+                int sum = 0;
+                while (number > 0)
+                {
+                    int num1 = number % 10;
+                    number /= 10;
+                    sum += num1;
+                }
+                return sum;
+            }
+
+            Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Сумма цифр равна {SumNums(num)}");
+        }
+    }
+}
