1739c6f50844cd73d30f3a0a6ba08d2deebddf6f
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-14 12:17:19 +0500

    task25 homework

---
+++ task25/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите цикл, который принимает на вход 
+два числа (A и B) и возводит число A в натуральную 
+степень B.*/
+            int DegreeNum(int a, int b)
+            {
+                int degree = a;
+                for (int i = 2; i < b+1; i++)
+                {
+                    degree *= a;
+                }
+                return degree;
+            }
+
+            Console.WriteLine("Введите числo А:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите числo B:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} в степени {b} равно {DegreeNum(a,b)}");
+        }
+    }
+}
