512b88c46a2c63cd824f58f7ad3b2a94f60d7187
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-14 10:44:20 +0500

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите программу, которая 
+принимает на вход число N и выдаёт 
+произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+            int MultiplyNums(int number)
+            {
+                int multiply = 1;
+                while (number > 0)
+                {
+                    multiply *= number;
+                    number--;
+                }
+                return multiply;
+            }
+
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Произведение чисел равно {MultiplyNums(num)}");
+        }
+    }
+}
