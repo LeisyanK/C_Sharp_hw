d0c12b0892ec46ed5f5931a674bf353316e9e7ab
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-14 10:23:12 +0500

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите программу, которая 
+принимает на вход число (А) и выдаёт сумму чисел 
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+            int GetSumNums(int number)
+            {
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number;
+                    number--;
+                }
+                return sum;
+            }
+
+            Console.WriteLine("Введите число");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Сумма чисел равна {GetSumNums(num)}");
+        }
+    }
+}
