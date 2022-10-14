7e817059abae103f71db3a1b7b1dab6268cd6e3d
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-14 18:28:38 +0500

    task29 homework

---
+++ task29/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишите программу, которая задаёт массив 
+                из 8 элементов и выводит их на экран.
+            */
+            int[] FillArray(int count)
+            {
+                int[] array = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    array[i] = new Random().Next(0,100);
+                }
+                return array;
+            }
+            void PrintArray(int[] arr)
+            {
+                Console.Write("[");
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    Console.Write($"{arr[i]} ");    
+                }                
+                Console.WriteLine($"{arr[arr.Length - 1]}]");
+            }
+
+            int[] array1 = FillArray(8);
+            PrintArray(array1);
+        }
+    }
+}
