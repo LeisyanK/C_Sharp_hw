142bc3f5fc0ef4cf465312f7ebe379ce3cceb50f
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-17 20:50:25 +0500

    task31 find sums of positive and negative elements

---
+++ task31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9].
+            Найдите суммы отрицательных и положительных элементов массива.*/
+            int[] GetArray(int size, int minValue, int maxValue)
+            {
+                int[] result = new int[size];
+                for (int i=0; i < size; i++)
+                {
+                    result[i] = new Random().Next(minValue, maxValue + 1);
+                    Console.Write($"{result[i]} ");
+                }
+                return result;
+            }
+
+            int[] array = GetArray(12, -9, 9);
+            int positiveSum = 0;
+            int negativeSum = 0;
+            foreach (int el in array)
+            {
+                if (el > 0) positiveSum += el;
+                else negativeSum += el;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сумма положительных чисел = {positiveSum}, сумма отрицательных чисел = {negativeSum}");
+        }
+    }
+}
