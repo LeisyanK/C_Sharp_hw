d5c7ac5c7f35db913ead444e8142da5ab032f260
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-10-25 19:41:23 +0500

    task41: count positive numbers

---
+++ task41/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь*/
+Main();
+
+void Main()
+{
+    int M = EnterNumber();
+    Console.WriteLine($"Количество положительных чисел: {PositiveCount(M)}");
+}
+
+int EnterNumber()
+            {
+                Console.Write("Введите количество чисел: ");
+                return int.Parse(Console.ReadLine());
+            }
+
+int PositiveCount(int number)
+{
+    int count = 0;
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите {i+1}-ое число: ");
+        int num = int.Parse(Console.ReadLine());
+        if(num > 0) count ++;
+    }
+    return count;
+}
