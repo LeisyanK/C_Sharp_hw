cced73ae50054a90bb027c2714f3de3e0bb6f398
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-11-09 00:39:24 +0500

    task57: we count how many times the elements repeat

---
+++ task57/Program.cs
@@ -0,0 +1,61 @@
+﻿// Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Другой способ решения: задавать интервал для чисел, которыми заполняем массив, и в цикле с параметром пробегаем этот интервал.
+Main();
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите количество строк: ");
+    int rows = int.Parse(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int columns = int.Parse(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    for (int z = 1; z < 10; z++)
+    {
+        int countNumber = CountNum(array, z);
+        if (countNumber > 0)
+        {
+            Console.WriteLine($"Число {z} встречается {countNumber} раз");
+        }
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int CountNum(int[,] array, int number)
+{
+    int count = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == number) count++;
+        }
+    }
+    return count;
+}
