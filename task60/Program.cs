b5c8a486c60431fcd862e3a2b8c5f659b3cbf055
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-11-17 03:32:41 +0500

    task60: three-dimentional array with non-repeated elements

--- task60/Program.cs
+++ task60/Program.cs
@@ -24,13 +24,15 @@ void Main()
 
 void FillArray(int[,,] array)
 {
+    int count = 10;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                 array[i, j, k] = new Random().Next(1, 10);  
+                 array[i, j, k] = count; //new Random().Next(1, 10);  
+                 count++;
             }
         }
     }
