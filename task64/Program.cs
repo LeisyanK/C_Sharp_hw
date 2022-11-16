784f9bc5ae4cf7dbf7fce803031c96796c035858
Kashapova Leisyan <LeisyanRA@mail.ru>
2022-11-17 03:21:11 +0500

    task64: even elements N..1

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "4, 2"
+N = 8 -> "8, 6, 4, 2,"
+*/
+Main();
+
+void Main()
+{
+    Console.Write("Введите число: ");
+    int n = int.Parse(Console.ReadLine());
+    if (n % 2 == 0) PrintNumber(n);
+    else PrintNumber(n-1);
+}
+
+void PrintNumber(int n)
+{
+    if (n <= 2) Console.WriteLine(n);
+    else
+    {
+        Console.Write(n + " ");
+        PrintNumber(n-2);
+    }
+}
